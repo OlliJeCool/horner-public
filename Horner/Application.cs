8c090f36813b5eb89c17b1e35edec192bc9453c6
Oliver Šulc <1001@student.itgymnazium.cz>
2021-11-04 16:52:22 +0100

    base function working

---
+++ Horner/Application.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Horner
+{
+    class Application
+    {
+        public void Run()
+        {
+            var horner = new Horner();
+            horner.SolveHorner();
+        }
+    }
+}
---
+++ Horner/Horner.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Horner
+{
+    class Horner
+    {
+
+        public double[] SolveHorner()
+        {
+            double[] input = new double[] { 1, 4, 6, 4, 1 };
+            double[] divisors = new double[] { 1, -1 };
+            double[] shorter = new double[input.Length];
+            for (int i = 0; i < divisors.Length; i++)
+            {
+                double temp = 1;
+                for (int j = 1; j < input.Length; j++)
+                {
+                   temp = (temp * divisors[i]) + input[j];
+                }
+                if(temp == 0)
+                {
+                    Console.WriteLine(divisors[i]);
+                }
+            }
+            return divisors;
+        }
+    }
+}
